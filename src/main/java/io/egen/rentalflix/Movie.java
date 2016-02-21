f9615c8e12d6ebdae84b189248020893e5d53047
HarmeetSingh24 <assi.harmeet24@gmail.com>
2016-02-21 02:17:00 -0600

    movie commit

--- src/main/java/io/egen/rentalflix/Movie.java
+++ src/main/java/io/egen/rentalflix/Movie.java
@@ -1,4 +1,6 @@
-package io.egen.rentalflix;
+package main.java.io.egen.rentalflix;
+
+
 
 /**
  * Entity representing a movie.
@@ -6,4 +8,33 @@ package io.egen.rentalflix;
  */
 public class Movie {
 	//POJO IMPLEMENTATION GOES HERE
+	private  int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	private String title;
+	private String year;
+	private String language;
 }
+
